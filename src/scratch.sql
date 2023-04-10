29696722e10b34d62c68e3decf810c0f89f35c15
Alex <borovinskihalex986@gmail.com>
2023-04-10 18:54:18 +0500

    V PSQL2.0

---
+++ src/scratch.sql
@@ -0,0 +1,35 @@
+create TABLE scypro
+(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    gender VARCHAR (6) NOT NULL,
+    age INT NOT NULL
+
+);
+
+INSERT INTO scypro(first_name, last_name, gender, age) VALUES('Alex','Morph','m',30);
+INSERT INTO scypro(first_name,last_name,gender,age) VALUES ('Lisa','Simpson','w',29);
+INSERT INTO scypro(first_name,last_name,gender,age) VALUES ('Eric','Cartman','m',11);
+INSERT INTO scypro(first_name,last_name,gender,age) VALUES ('Tiest','Fiesto','m',25);
+INSERT INTO scypro(first_name,last_name,gender,age) VALUES ('Anna','Smith','w',50);
+
+SELECT * FROM scypro;
+
+SELECT first_name AS Имя,last_name AS Фамилия FROM scypro;
+
+SELECT * FROM scypro WHERE age < 30 OR age > 50;
+
+SELECT * FROM scypro WHERE age BETWEEN 30 AND 50;
+
+
+SELECT last_name AS Фамилия FROM scypro ORDER BY last_name DESC;
+SELECT * FROM scypro WHERE first_name LIKE '_ _ _ _ _';
+
+UPDATE scypro SET first_name = 'Alex' WHERE id = 3;
+UPDATE scypro SET first_name = 'Alex' WHERE id = 4;
+
+SELECT first_name AS имя ,SUM(age) AS суммарный_возраст  FROM scypro GROUP BY имя;
+--
+SELECT last_name age FROM scypro WHERE age = (SELECT MIN(age) FROM scypro);
+SELECT first_name AS  имя, MAX(age) AS max  FROM scypro GROUP BY first_name HAVING COUNT(first_name)>2;
