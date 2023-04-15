547512fb0f068440e281afdf23c5b6b7cc774eed
Alex <borovinskihalex986@gmail.com>
2023-04-15 21:23:43 +0500

    V PSQL2.4

--- src/scratch.sql
+++ src/scratch.sql
@@ -26,20 +26,20 @@ SELECT * FROM scypro WHERE age BETWEEN 30 AND 50;
 
 
 SELECT last_name AS Фамилия FROM scypro ORDER BY last_name DESC;
-SELECT * FROM scypro WHERE LENGHT(first_name) >4;
+SELECT * FROM scypro WHERE length(first_name) >4;
 
 UPDATE scypro SET first_name = 'Alex' WHERE id = 3;
 UPDATE scypro SET first_name = 'Alex' WHERE id = 4;
 
 SELECT first_name AS имя ,SUM(age) AS суммарный_возраст  FROM scypro GROUP BY имя;
 
-SELECT first_name AS имя,MIN(age) AS min_возраст FROM scypro GROUP BY first_name ,age LIMIT 1;
+SELECT first_name AS имя,MIN(age) AS min_возраст FROM scypro  GROUP BY имя ;
 
-SELECT * FROM scypro ORDER BY age LIMIT 1;
 
-SELECT first_name, age FROM scypro ORDER BY age LIMIT 1;
+SELECT first_name, age FROM scypro ORDER BY age;
 
 SELECT first_name,age FROM scypro WHERE age = (SELECT MIN(age) FROM scypro) GROUP BY age, first_name ;
 
 --
 SELECT first_name AS  имя, MAX(age) AS max  FROM scypro GROUP BY first_name HAVING COUNT(first_name)>2;
+-----------------------------------------------------
